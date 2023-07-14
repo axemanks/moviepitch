f804e56a448fc650a08fbf89d4e8fbbc8a4d56fc
Keith Scheldt <keithscheldt@gmail.com>
2023-07-14 19:53:06 -0400

    added prompts

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import { process } from '/env.js'
 import { Configuration, OpenAIApi } from 'openai'
 
-const setupTextarea = document.getElementById('setup-textarea')// text area on setup page
+
 const setupInputContainer = document.getElementById('setup-input-container') // input
 const movieBossText = document.getElementById('movie-boss-text') // the prompt text
 
@@ -16,15 +16,16 @@ const openai = new OpenAIApi(configuration)
 
 // add event listener to the send button
 document.getElementById("send-btn").addEventListener("click", () => {
+    const setupTextarea = document.getElementById('setup-textarea')// text area on setup page
     // if there is input when the button is clicked...
-  if (setupTextarea.value) {
-    const userInput = setupTextarea.value // get the input
-    setupInputContainer.innerHTML = `<img src="images/loading.svg" class="loading" id="loading">` // display loading
-    movieBossText.innerText = `Ok, just wait a second while my digital brain digests that...` // update the text while loading
-   // fetch the response from the API
-    fetchBotReply(userInput);
-    fetchSynopsis(userInput);
-}
+    if (setupTextarea.value) {
+        const userInput = setupTextarea.value // get the input
+        setupInputContainer.innerHTML = `<img src="images/loading.svg" class="loading" id="loading">` // display loading
+        movieBossText.innerText = `Ok, just wait a second while my digital brain digests that...` // update the text while loading
+        // fetch the response from the API
+        fetchBotReply(userInput);
+        //fetchSynopsis(userInput);
+    }
 });
 
 
@@ -32,11 +33,24 @@ document.getElementById("send-btn").addEventListener("click", () => {
 async function fetchBotReply(outline) {
     const response = await openai.createCompletion({
         model: 'text-davinci-003',
-        prompt: `Generate an engaging, professional and marketable movie synopsis based on the following idea: ${outline}`,
+        prompt: `Generate a short message to enthusiastically say an outline sounds interesting and that you need some minutes to think about it.
+        ###
+        outline: Two dogs fall in love and move to Hawaii to learn to surf.
+        message: I'll need to think about that. But your idea is amazing! I love the bit about Hawaii!
+        ###
+        outline: A plane crashes in the jungle and the passengers have to walk 1000km to safety.
+        message: I'll spend a few moments considering that. But I love your idea!! A disaster movie in the jungle!
+        ###
+        outline: A group of corrupt lawyers try to send an innocent woman to jail.
+        message: Wow that is awesome! Corrupt lawyers, huh? Give me a few moments to think!
+        ###
+        outline: ${outline}
+        message: 
+        `,
         max_tokens: 60 // default is 16
     });
-    
-    movieBossText.innerText = response.data.choices[0].text.trim()
+
+    movieBossText.innerText = response.data.choices[ 0 ].text.trim()
     console.log(response)
 };
 
@@ -44,10 +58,17 @@ async function fetchBotReply(outline) {
 async function fetchSynopsis(outline) {
     const response = await openai.createCompletion({
         model: 'text-davinci-003',
-        prompt: `Generate a short synopsis for a movie based on the outline supplied by the user: "${outline}"`,
+        prompt: `Generate an engaging, professional and marketable movie synopsis based on an outline
+        ###
+        outline: A big-headed daredevil fighter pilot goes back to school only to be sent on a deadly mission.
+        synopsis: The Top Gun Naval Fighter Weapons School is where the best of the best train to refine their elite flying skills. When hotshot fighter pilot Maverick (Tom Cruise) is sent to the school, his reckless attitude and cocky demeanor put him at odds with the other pilots, especially the cool and collected Iceman (Val Kilmer). But Maverick isn't only competing to be the top fighter pilot, he's also fighting for the attention of his beautiful flight instructor, Charlotte Blackwood (Kelly McGillis). Maverick gradually earns the respect of his instructors and peers - and also the love of Charlotte, but struggles to balance his personal and professional life. As the pilots prepare for a mission against a foreign enemy, Maverick must confront his own demons and overcome the tragedies rooted deep in his past to become the best fighter pilot and return from the mission triumphant.
+        ###
+        outline:${outline}
+        synopsis:
+        `,
         max_tokens: 800
     });
 
-    document.getElementById('output-text').innerText = response.data.choices[0].text.trim()
+    document.getElementById('output-text').innerText = response.data.choices[ 0 ].text.trim()
 };
 
