cc32e292218de0b2a5323d56d16b769067493a77
Keith Scheldt <keithscheldt@gmail.com>
2023-07-14 20:28:57 -0400

    added stars and synopsis

--- index.js
+++ index.js
@@ -24,7 +24,8 @@ document.getElementById("send-btn").addEventListener("click", () => {
         movieBossText.innerText = `Ok, just wait a second while my digital brain digests that...` // update the text while loading
         // fetch the response from the API
         fetchBotReply(userInput);
-        //fetchSynopsis(userInput);
+        fetchSynopsis(userInput);
+
     }
 });
 
@@ -58,7 +59,7 @@ async function fetchBotReply(outline) {
 async function fetchSynopsis(outline) {
     const response = await openai.createCompletion({
         model: 'text-davinci-003',
-        prompt: `Generate an engaging, professional and marketable movie synopsis based on an outline
+        prompt: `Generate an engaging, professional and marketable movie synopsis based on an outline. The synopsis should include actors names in brackets after each character. Choose actors that would be ideal for this role.
         ###
         outline: A big-headed daredevil fighter pilot goes back to school only to be sent on a deadly mission.
         synopsis: The Top Gun Naval Fighter Weapons School is where the best of the best train to refine their elite flying skills. When hotshot fighter pilot Maverick (Tom Cruise) is sent to the school, his reckless attitude and cocky demeanor put him at odds with the other pilots, especially the cool and collected Iceman (Val Kilmer). But Maverick isn't only competing to be the top fighter pilot, he's also fighting for the attention of his beautiful flight instructor, Charlotte Blackwood (Kelly McGillis). Maverick gradually earns the respect of his instructors and peers - and also the love of Charlotte, but struggles to balance his personal and professional life. As the pilots prepare for a mission against a foreign enemy, Maverick must confront his own demons and overcome the tragedies rooted deep in his past to become the best fighter pilot and return from the mission triumphant.
@@ -68,7 +69,38 @@ async function fetchSynopsis(outline) {
         `,
         max_tokens: 800
     });
-
-    document.getElementById('output-text').innerText = response.data.choices[ 0 ].text.trim()
+    const synopsis = response.data.choices[ 0 ].text.trim()
+    document.getElementById('output-text').innerText = synopsis
+    fetchTitle(synopsis)
+    fetchStars(synopsis)
 };
+
+// fetch title
+async function fetchTitle(synopsis) {
+    const response = await openai.createCompletion({
+        model: 'text-davinci-003',
+        prompt: `Generate a gripping and exciting title for a movie that will go down in history based on the ${synopsis}`,
+        max_tokens: 50,
+        temperature: 0.7,// more creative
+    });
+    document.getElementById('output-title').innerText = response.data.choices[ 0 ].text.trim() // change "output-title" h1
+};
+
+// extract actor names
+async function fetchStars(synopsis) {
+    const response = await openai.createCompletion({
+        model: 'text-davinci-003',
+        prompt: `Extract the names in brackets from the synopsis.
+        ###
+        synopsis: The Top Gun Naval Fighter Weapons School is where the best of the best train to refine their elite flying skills. When hotshot fighter pilot Maverick (Tom Cruise) is sent to the school, his reckless attitude and cocky demeanor put him at odds with the other pilots, especially the cool and collected Iceman (Val Kilmer). But Maverick isn't only competing to be the top fighter pilot, he's also fighting for the attention of his beautiful flight instructor, Charlotte Blackwood (Kelly McGillis). Maverick gradually earns the respect of his instructors and peers - and also the love of Charlotte, but struggles to balance his personal and professional life. As the pilots prepare for a mission against a foreign enemy, Maverick must confront his own demons and overcome the tragedies rooted deep in his past to become the best fighter pilot and return from the mission triumphant.
+        names: Tom Cruise, Val Kilmer, Kelly McGillis
+        ###
+        synopsis: ${synopsis}
+        names:   
+        `,
+        max_tokens: 30
+    })
+    document.getElementById('output-stars').innerText = response.data.choices[ 0 ].text.trim()
+}
+
 
